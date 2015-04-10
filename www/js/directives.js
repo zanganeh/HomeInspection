e0cfe2bf9671de4bd5c4a9d62a01493b187c201a
zanganeh <zanganeh@gmail.com>
2015-04-10 20:04:23 +1000

    upgrade

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,33 +1,5 @@
 ﻿angular.module('starter.directives', ['ui.bootstrap'])
-.directive('hiRating', function ()
-{
-	function link($scope, element, attrs)
-	{
-		$scope.max = 10;
-		$scope.isReadonly = false;
 
-		$scope.hoveringOver = function (value)
-		{
-			$scope.overStar = value;
-			$scope.percent = 100 * (value / $scope.max);
-		};
-
-		$scope.ratingStates = [
-		  { stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle' },
-		  { stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty' },
-		  { stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle' },
-		  { stateOn: 'glyphicon-heart' },
-		  { stateOff: 'glyphicon-off' }
-		];
-	}
-	return {
-		templateUrl: 'templates/hi-rating.html',
-		link: link,
-		scope: {
-			rate: '=rate'
-		}
-	};
-})
 .directive('inspectionDetail', function (dbService)
 {
 	function link($scope, element, attrs, form)
